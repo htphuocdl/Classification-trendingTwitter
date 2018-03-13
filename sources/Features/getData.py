5cc14026fded8cc6590c469a997a7f4cb918f87f
crazytrau <htphuocdl@gmail.com>
2018-03-13 16:38:42 +0700

    log json to file

--- sources/Features/getData.py
+++ sources/Features/getData.py
@@ -1,7 +1,14 @@
 from myId import *
 from googletrans import Translator
 import tweepy
-import csv
+import json
+
+# Make it work for Python 2+3 and with Unicode
+import io
+try:
+    to_unicode = unicode
+except NameError:
+    to_unicode = str
 
 def process_dir(data_path):
     # get candidates
@@ -29,28 +36,30 @@ if __name__ == "__main__":
     auth.set_access_token(access_token,access_secret)
     api = tweepy.API(auth)
 
-    with open('output.csv','w',newline='') as csvfile:
-        fieldnames = ['tweetId','userId','tweet','retweet_count','arr_hashtags','links','isReplies','created','lang']
-        writer = csv.DictWriter(csvfile,fieldnames=fieldnames,delimiter='\t')
-        writer.writeheader()
+    data = {}
 
-        with open('../../../TT-classification/tweets/0a14dee099e100e8bf356a9937a6f974', 'r') as file:
-            for line in file:
-                print (line.split()[0])
-                try:
-                    _id = line.split()[0]
-                    res = api.get_status(_id)._json
-                    writer.writerow({   'tweetId':line.split()[0],
-                                        'userId':res['user']['id'],
-                                        'tweet':translator.translate(res['text']).text,
-                                        'retweet_count':res['retweet_count'],
-                                        'arr_hashtags':res['entities']['hashtags'],
-                                        'links':len(res['entities']['urls']),
-                                        'isReplies':isReplies(res['in_reply_to_status_id']),
-                                        'created':res['created_at'],
-                                        'lang':res['lang']})
-                except tweepy.TweepError:
-                    print ('---------------------')
+    with open('../../../TT-classification/tweets/0a14dee099e100e8bf356a9937a6f974', 'r') as file:
+        for line in file:
+            print (line.split()[0])
+            try:
+                _id = line.split()[0]
+                res = api.get_status(_id)._json
+                data[_id] ={       'userId':res['user']['id'],
+                                'tweet':translator.translate(res['text']).text,
+                                'retweet_count':res['retweet_count'],
+                                'arr_hashtags':res['entities']['hashtags'],
+                                'links':len(res['entities']['urls']),
+                                'isReplies':isReplies(res['in_reply_to_status_id']),
+                                'created':res['created_at'],
+                                'lang':res['lang']}
+            except tweepy.TweepError:
+                print ('---------------------')
+    # Write JSON file
+    with io.open('output.json', 'w', encoding='utf8') as outfile:
+        str_ = json.dumps(data,
+                          indent=4, sort_keys=True,
+                          separators=(',', ': '), ensure_ascii=False)
+        outfile.write(to_unicode(str_))
 # auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 # auth.set_access_token(access_token,access_secret)
 # api = tweepy.API(auth)
