46593201ed09e2237dfdbce1d6bd76b7b6ac16fb
crazytrau <htphuocdl@gmail.com>
2018-03-13 15:58:52 +0700

    write data csv

--- sources/Features/getData.py
+++ sources/Features/getData.py
@@ -1,28 +1,73 @@
 from myId import *
+from googletrans import Translator
 import tweepy
+import csv
 
-def define(num): #if have return 1
-    if num==0 or num==None:
-        return 0
-    else:
+def process_dir(data_path):
+    # get candidates
+    lst_input = []
+    for filename in os.listdir(data_path):
+        if filename.endswith('.txt'):
+            lst_input.append(filename)
+    return lst_input
+
+def main(data_path):
+    lst_input = process_dir(data_path)
+    for file in lst_input:
+        with open(os.path.join(data_path,file),'r') as f:
+            lines = f.readlines()
+
+def isReplies(a):
+    if a and a>0:
         return 1
+    else:
+        return 0
 
 if __name__ == "__main__":
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    translator = Translator()
+    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
     auth.set_access_token(access_token,access_secret)
     api = tweepy.API(auth)
 
-    res = api.get_status(972713980824186880)._json
-    print (res)
-    vector = [  {'level retweeted':res['retweet_count']}, #chua lam duoc
-                {'is retweeted': define(res['retweet_count'])},
-                {'num hashtags':len(res['entities']['hashtags'])},
-                {'Length of tweet':len(res['text'])},
-                {'Exclamations': define('!' in res['text'])},
-                {'Question': define('!' in res['text'])},
-                {'links':len(res['entities']['urls'])},
-                {'Topic repetition':'/'}, #chua hieu
-                {'Replies':define(res['in_reply_to_status_id'])},
-                {'Time':res['created_at']}
-            ]
-    print (vector)
+    with open('output.csv','w',newline='') as csvfile:
+        fieldnames = ['tweetId','userId','tweet','retweet_count','arr_hashtags','links','isReplies','created','lang']
+        writer = csv.DictWriter(csvfile,fieldnames=fieldnames,delimiter='\t')
+        writer.writeheader()
+
+        with open('../../../TT-classification/tweets/0a14dee099e100e8bf356a9937a6f974', 'r') as file:
+            for line in file:
+                print (line.split()[0])
+                try:
+                    _id = line.split()[0]
+                    res = api.get_status(_id)._json
+                    writer.writerow({   'tweetId':line.split()[0],
+                                        'userId':res['user']['id'],
+                                        'tweet':translator.translate(res['text']).text,
+                                        'retweet_count':res['retweet_count'],
+                                        'arr_hashtags':res['entities']['hashtags'],
+                                        'links':len(res['entities']['urls']),
+                                        'isReplies':isReplies(res['in_reply_to_status_id']),
+                                        'created':res['created_at'],
+                                        'lang':res['lang']})
+                except tweepy.TweepError:
+                    print ('---------------------')
+# auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+# auth.set_access_token(access_token,access_secret)
+# api = tweepy.API(auth)
+# try:
+#     res = api.get_status(42659021370568704)
+# except tweepy.TweepError:
+#     print('a')
+
+
+    # trends1 = api.trends_place(1) # from the end of your code
+    # # trends1 is a list with only one element in it,which is a
+    # # dict which we'll put in data.
+    # data = trends1[0]
+    # # grab the trends
+    # trends = data['trends']
+    # # grab the name from each trend
+    # names = [trend['name'] for trend in trends]
+    # # put all the names together with a ' ' separating them
+    # trendsName = '\n'.join(names)
+    # print(trendsName)
