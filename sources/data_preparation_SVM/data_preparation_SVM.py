570b87a2c3e78a560ef51e1eab0da51bca66f6a0
crazytrau <htphuocdl@gmail.com>
2018-03-31 01:05:22 +0700

    edit adap to windows

--- sources/data_preparation_SVM/data_preparation_SVM.py
+++ sources/data_preparation_SVM/data_preparation_SVM.py
@@ -6,17 +6,25 @@ Created on Tue Mar 20 19:56:36 2018
 @author: crazytrau
 """
 # import library
+import json
 import csv
-import sys
-sys.path.insert(0, '../Features/')
 from getData import getDataHashCode
 
 #main function
 if __name__ == '__main__':
     # main variable
     trendingTopicArr=[]
+
+    data = json.load(open('output.json'))
+
+    toCur = False
+
     #read every trending and export json file
-    with open('../../data/TT-annotations.csv', newline='') as csvfile:
+    with open('../../data/TT-annotations.csv', newline='', encoding="utf8") as csvfile:
         trendingTopicArr = csv.reader(csvfile, delimiter=';')
         for trendingTopic in trendingTopicArr:
-            getDataHashCode(trendingTopic[0])
+            print (trendingTopic[0], data)
+            if (toCur == True):
+                num = getDataHashCode(trendingTopic[0])
+            if (trendingTopic[0] == data):
+                toCur = True
---
+++ sources/data_preparation_SVM/getData.py
@@ -0,0 +1,73 @@
+from myId import *
+import os
+from googletrans import Translator
+import tweepy
+import json
+
+# Make it work for Python 2+3 and with Unicode
+import io
+try:
+    to_unicode = unicode
+except NameError:
+    to_unicode = str
+
+def process_dir(data_path):
+    # get candidates
+    lst_input = []
+    for filename in os.listdir(data_path):
+        if filename.endswith('.txt'):
+            lst_input.append(filename)
+    return lst_input
+
+def main(data_path):
+    lst_input = process_dir(data_path)
+    for file in lst_input:
+        with open(os.path.join(data_path,file),'r') as f:
+            lines = f.readlines()
+
+def isReplies(a):
+    if a and a>0:
+        return 1
+    else:
+        return 0
+
+def getDataHashCode(trendingHashCode):
+    translator = Translator()
+    auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+    auth.set_access_token(access_token,access_secret)
+    api = tweepy.API(auth)
+
+    data = {}
+    temp=0
+    temp1=0
+    with open('../../data/tweets/'+trendingHashCode, 'r') as file:
+        for line in file:
+            print (temp, temp1, line.split()[0])
+            temp+=1
+            try:
+                _id = line.split()[0]
+                res = api.get_status(_id)._json
+                data[_id] ={    'userId':res['user']['id'],
+                                'tweet':translator.translate(res['text']).text,
+                                'retweet_count':res['retweet_count'],
+                                'arr_hashtags':res['entities']['hashtags'],
+                                'links':len(res['entities']['urls']),
+                                'isReplies':isReplies(res['in_reply_to_status_id']),
+                                'created':res['created_at'],
+                                'lang':res['lang']}
+                temp1+=1
+            except tweepy.TweepError:
+                print ('---------------------')
+    #Write JSON file
+
+    str_ = json.dumps(data,
+                        indent=4, sort_keys=False,
+                        separators=(',', ': '), ensure_ascii=False)
+    filename='../data_preparation_SVM/features/'+trendingHashCode+'.json'
+    file = open(filename, "w", encoding='utf-8')
+    file.write(str_)
+    file.close()
+    filename='../data_preparation_SVM/output.json'
+    file = open(filename, "w", encoding='utf-8')
+    file.write('"'+ trendingHashCode+'"')
+    file.close()
