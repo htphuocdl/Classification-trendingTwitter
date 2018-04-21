29f1bd9a4fb0759749df5c2a5cd982fea71ec4d5
crazytrau <htphuocdl@gmail.com>
2018-04-21 12:10:02 +0700

    svm train/ test

---
+++ sources/data_preparation_SVM/train_SVM.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 17 12:46:50 2018
+
+@author: crazytrau
+"""
+# import library
+import json
+import csv
+from generateFeature import generateFeature
+
+#main function
+if __name__ == '__main__':
+    # main variable
+    trendingTopicArr=[]
+
+    data = json.load(open('output.json'))
+    arrVectors = []
+
+    toCur = False
+
+    #read every trending and export json file
+    with open('../../data/TT-annotations.csv', newline='', encoding="utf8") as csvfile:
+        trendingTopicArr = csv.reader(csvfile, delimiter=';')
+        for trendingTopic in trendingTopicArr:
+            path='../../../features/'+trendingTopic[0]+'.json'
+            temp = generateFeature(trendingTopic[2], path, trendingTopic[3])
+#            print(trendingTopic[3])
+            if temp != None: arrVectors.append(temp)
+            
+    columns = [    'depth_retweets',
+                  'ratio_retweets',
+                  'hashtags',
+                  'length',
+                  'exclamations',
+                  'questions',
+                  'links',
+                  'topicRepetition',
+                  'replies',
+                  'spreadVelocity',
+                  'user_diversity',
+                  'retweeted_user_diversity',
+                  'hashtag_diversity',
+                  'language_diversity',
+                  'vocabulary_diversity',
+                  'class']
+            
+# Support Vector Machine (SVM)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.DataFrame(arrVectors, columns=columns)
+X = dataset.iloc[:,0:-1].values
+y = dataset.iloc[:, 15].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear')
+classifier.fit(X_train, y_train)
+7
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix,cohen_kappa_score
+#cm = confusion_matrix(y_test, y_pred, labels=[0,1])
+#cohen_kappa_score(y_test, y_pred, labels=[0,1])
+cm = confusion_matrix(y_test, y_pred)
+cohen_kappa_score(y_test, y_pred)
+
+a=0
+for i in range(0,len(y_pred)):
+    if y_pred[i] == y_test[i]:
+        a=a+1
+print (a/len(y_pred))
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
